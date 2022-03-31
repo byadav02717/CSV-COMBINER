b11c95540d2855c6ab099bc066670182b90fc230
Bivash Yadav <byadav02717@gmail.com>
2022-03-31 18:27:33 -0500

    added some comments

--- csv-combiner.py
+++ csv-combiner.py
@@ -1,15 +1,24 @@
 import sys
 
 argc = len(sys.argv)
+#variable to track if it is the first line of output
 firstline = True
 for i in range(1,argc):
+    #spliting path into array. filename will be at last index
     pathname = sys.argv[i].split("/")
     filename = pathname[len(pathname)-1]
+
+    #opeing file
     file = open(sys.argv[i], 'r')
+    
+    # reading first line
     line = file.readline()
-    if(firstline):
+
+    #printing first line
+    if(firstline and line):
         print("{},{}".format(line.strip(), "\"filename\""))
         firstline = False
+    #reading other lines in the file
     line = file.readline()
     while(line):
         print("{},\"{}\"".format(line.strip(), filename))
